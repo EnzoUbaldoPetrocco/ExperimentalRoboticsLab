1edfa796ad3b5ad9079947bf11dce2410f3d70da
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2021-11-16 23:05:39 +0100

    armor_api

--- build/CMakeFiles/_ExperimentalRoboticsLab_generate_messages_check_deps_TrySolution.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/_ExperimentalRoboticsLab_generate_messages_check_deps_TrySolution"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/_ExperimentalRoboticsLab_generate_messages_check_deps_TrySolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
