fb045a43d0f9db56be7d2692a1bb284f9003e79b
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2022-05-09 18:22:57 +0200

    only a little modification

--- short.sh
+++ short.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-
 gnome-terminal --tab --title="assignment2" -- bash -c "source ros.sh; roslaunch ExperimentalRoboticsLab assignment2_short.launch"
-gnome-terminal --tab --title="armor" -- bash -c "source ros.sh; rosrun armor execute it.emarolab.armor.ARMORMainService"
+gnome-terminal --tab --title="armor" -- bash -c "sleep 5; source ros.sh; rosrun armor execute it.emarolab.armor.ARMORMainService"
 gnome-terminal --tab --title="investigation" -- bash -c "sleep 15; source ros.sh; roslaunch ExperimentalRoboticsLab investigation.launch"
 gnome-terminal --tab --title="simulation" -- bash -c "sleep 15; source ros.sh; roslaunch ExperimentalRoboticsLab simulation.launch;"
 gnome-terminal --tab --title="rosplan" -- bash -c "sleep 15; source ros.sh; roslaunch ExperimentalRoboticsLab rosplan.launch"
